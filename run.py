4837bc1d82b43cd1df3f77e37944fa153969af18
adrianleung5 <adrianleung50@gmail.com>
2022-12-28 17:27:51 +0000

    seperate numbers by commas

--- run.py
+++ run.py
@@ -14,14 +14,13 @@ SHEET = GSPREAD_CLIENT.open('Love_Sandwiches')
 
 
 def get_sales_data():
- """
-  Get sales figures input from the user.
- """
- print("Please enter sales data from the last market.")
- print("Data should be six numbers, separated by commas.")
- print("Example: 10,20,30,40,50,60\n")
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
- data_str = input("Enter yout data here:")
- print(f"The data provided is {data_str}")
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
 
-get_sales_data()
